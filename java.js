9cb1c6f97eed4978b0039d716fa28e571873cd8a
Nyadav123 <89506943+Nyadav123@users.noreply.github.com>
2022-01-17 22:53:15 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+$(".row3").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+});
+$(".row2").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+});
